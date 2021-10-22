dde615bc64a10af8ba41f209731652747e46f28a
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2021-10-21 21:17:08 -0400

    typed in my written work for Kiosk.java

--- Kiosk.java
+++ Kiosk.java
@@ -1,2 +1,48 @@
+//TOOK AROUND 10 minutes to type all of this 
+import java.util.Scanner;
+public class Kiosk{
+  //main method and user interface
+  int[] purchased = new int[4];
+  int counter = 0;
+  public static void main(String args[]){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Welcome to Dan's Beverage store!");
+    System.out.println("Here is what you can buy:");
+    System.out.println("\tApplejuice($2)");
+    System.out.println("\tSprite($3)");
+    System.out.println("\tCaprisun($1)");
+    System.out.println("\tGatorade($2)");
+    System.out.println("\tWater($1)");
+    Ask();
+    Printer print = new Printer(purchased, counter);
+  }
+//asks user for purchases
+  public static void Ask(){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How much Applejuice do you wanna buy?: ");
+    purchased[0] = scan.nextInt();
+    counter+=purchased[0];
 
-//your main interface for the project, with the main method
+    System.out.println("How much Sprite do you wanna buy?: ");
+    purchased[1] = scan.nextInt();
+    counter+=purchased[1];
+
+    System.out.println("How much Caprisun do you wanna buy?: ");
+    purchased[2] = scan.nextInt();
+    counter+=purchased[2];
+
+    System.out.println("How much Gatorade do you wanna buy?: ");
+    purchased[3] = scan.nextInt();
+    counter+=purchased[3];
+
+    System.out.println("How much Water do you wanna buy?: ");
+    purchased[4] = scan.nextInt();
+    counter+=purchased[4];
+
+    if(counter>9){
+        counter = 0;
+        Ask();
+    }
+
+  }
+}
