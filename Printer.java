7db3ebb15e0ae7a7c8b70b269c9660abc816b26d
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2021-10-21 21:18:23 -0400

    typed written work for Printer.java

--- Printer.java
+++ Printer.java
@@ -1,21 +1,27 @@
-//Printer.java
+//THIS TOOK ABOUT 10 MINUTES TO TYPE ALTOGETHER (KIOSK INCLUDED)
+public class Printer{
+    int[] items = new int[4];
+    //prints out receipt
+    double tax = .17;
+    int applePrice = 2;
+    int spritePrice = 3
+    int waterPrice = 1;
+    int caprisunPrice = 4;
+    int gatoradePrice = 5;
+    int total = 0;
+    String[] names = ["Applejuice", "Sprite", "Caprisun", "Gatorade", "Water"];
 
-/* This is a java file 'stub' or piece of a file designed to get you started on 
-* learning to code in java, specifically with multiple classes and methods that rely on the other.
-*
-*For this assignment, I would like you to fork, modify and commit this project, while
-* adding the following elements to your code.
-*
-* The receipt printer file could take arguments or parametrs from another class
-* This file will be purely for output
-****************************************
-* 40 characters is a standard width    * 
-* for typical receipt paper, your task *
-* is to make output from the screen    *
-* look like it is printing to a small  *
-* receipt type printer, with all the   *
-* text formatted to 40 characters wide *
-*                                      *
-* Have fun with the assignment!!       *
-****************************************
-*/
+    public Printer(int[] purchased, int counter){
+        items = purchased;
+        total = counter;
+    }
+
+    public void printReceipt(){
+        System.out.println("Quantity      Name                        Price");
+        for(int i = 0; i<5; i++){
+            if(items[i] != 0){
+                System.out.println("");
+            }
+        }
+    }
+}
