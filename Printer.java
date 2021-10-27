2dc9797df4d1eb8db9d63b1ff5c8a93feefaebf2
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2021-10-26 21:39:49 -0400

    made receipt line up better, easy to change values

--- Printer.java
+++ Printer.java
@@ -14,38 +14,47 @@ one for the names of the items, one for the prices, and one for the quantity bou
 - I also used subtraction to print a supplementary number of spaces so everything was lined up in the receipt
 - I then used the same use of for loops and those same arrays to make my Ask() function in the Kiosk class shorter
 - I then added finishing touches: printing out subtotal, total (with tax), made interface look nicer, etc.
+- During class, Twyford said to line up the subtotal, tax, and total, and also change the max items function and make it easier to change what items are available
+- So I did all that, creating another for loop to make sure everything lines up to the decimal point. 
 */
 
 //prints out receipt using synced arrays and for loops
 public class Printer{
     //use of arrays helps me more efficiently write code to print and store values
     int[] items = new int[5];
-    double [] prices = {2.00, 3.00, 4.00, 5.00, 1.00};
-    String[] names = {"Applejuice", "Sprite", "Caprisun", "Gatorade", "Water"};
+    double [] prices = new double[5];
+    String[] names = new String[5];
 	
-    double tax = .07;
+    double tax = 0;
     double subTotal = 0;
 //Printer object simply allows the user purchases to be passed through to this class
-    public Printer(int[] purchased){
+    public Printer(int[] purchased, double[] itemPrice, String[] itemName, double t){
         items = purchased;
+        prices = itemPrice;
+        names = itemName;
+        tax = t;
     }
 //this function handles the actual printing of the receipt, using for loops, arrays, and subtraction to get the job done
     public void printReceipt(){
         System.out.println("THANKS FOR SHOPPING AT DAN'S");
         System.out.println("HERE IS YOUR RECEIPT:");
         System.out.println("----------------------------------------");
-        System.out.println("Quantity      Name                 Price");
+        System.out.println("Qty   Name                        Price");
         System.out.println("----------------------------------------");
         //looping to display each beverage with their own data
         for(int i = 0; i<5; i++){
             if(items[i] != 0){
                 //uses subtraction to print supplementary amount of spaces (I call them dashes)
-                int spaces = 21 - names[i].length();
+                int spaces = 28 - names[i].length();
                 String dashes = "";
+                if(items[i]*prices[i] >=10){
+                    spaces-=1;
+                }
                 for(int j = 0; j<spaces; j++){
                     dashes = dashes + " "; 
                 }
-                System.out.println(items[i] + "             " + names[i] + dashes + "$"+ String.format("%.2f",items[i]*prices[i]));
+                
+                System.out.println(items[i] + "     " + names[i] + dashes + "$"+ String.format("%.2f",items[i]*prices[i]));
                 subTotal += items[i]*prices[i];
             }
         }
@@ -66,7 +75,7 @@ public class Printer{
         System.out.println(showTot+totDash+"$"+totVal);
     }
     public String spaces(String text, String val){
-		int spaces = (39-val.length()) - text.length();
+		int spaces = (38-val.length()) - text.length();
         String dashes = "";
         for(int j = 0; j<spaces; j++){
            dashes = dashes + " "; 
